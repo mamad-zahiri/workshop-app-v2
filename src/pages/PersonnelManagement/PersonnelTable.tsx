aff62b55d6a6dd267021e2ebf0a820d56f13b4cd
Mamad Zahiri <mamadzhri@gmail.com>
2024-10-08 17:03:03 +0330

    make mobile mode for personnel managemnent page

--- src/pages/PersonnelManagement/PersonnelTable.tsx
+++ src/pages/PersonnelManagement/PersonnelTable.tsx
@@ -8,7 +8,7 @@ const PersonnelActivitiesTableElm = ({
 }) => {
   return (
     <>
-      <div className='flex justify-start bg-slate-100 dark:bg-meta-4 border-b hover:[&>div]:bg-slate-400 hover:[&>div]:text-white'>
+      <div className=' hidden sm:flex justify-start bg-slate-100 dark:bg-meta-4 border-b hover:[&>div]:bg-slate-400 hover:[&>div]:text-white'>
         <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-30'>
           <h5 className='text-sm font-medium xsm:text-base'>کد پرسنلی</h5>
         </div>
@@ -21,68 +21,76 @@ const PersonnelActivitiesTableElm = ({
           <h5 className='text-sm font-medium xsm:text-base'>نام</h5>
         </div>
 
-        <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
+        {/* <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
           <h5 className='text-sm font-medium xsm:text-base'>دریافت شده</h5>
-        </div>
+        </div> */}
 
-        <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
+        {/* <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
           <h5 className='text-sm font-medium xsm:text-base'>بستانکاری</h5>
-        </div>
+        </div> */}
 
         <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
           <h5 className='text-sm font-medium xsm:text-base'>شماره تماس</h5>
         </div>
 
-        <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
+        {/* <div className='p-2 text-center sm:block xl:p-4 flex justify-center content-start w-40'>
           <h5 className='text-sm font-medium xsm:text-base'>کد ملی</h5>
-        </div>
+        </div> */}
       </div>
 
       {personnel.map((item, key) => (
         <div
-          className={`hover:bg-slate-200 dark:hover:bg-slate-700 flex justify-start ${
+          className={`flex-col sm:flex-row hover:bg-slate-200 dark:hover:bg-slate-700 flex justify-start ${
             key === personnel.length - 1
               ? ""
               : "border-b border-stroke dark:border-strokedark"
           }  gap-0`}
           key={key}>
-          <div className='p-2 text-center py-4 w-30'>
+          <div className='p-2 sm:text-center sm:py-4 w-full text-right sm:w-30'>
             <p className='text-black dark:text-white'>
+              <span className='inline-block sm:hidden me-2'>کد پرسنلی:</span>
+
               {item.personnelCode.toUpperCase()}
             </p>
           </div>
 
-          <div className='p-2 text-center py-4 w-40'>
-            <p className='text-black dark:text-white' dir='ltr'>
+          <div className='p-2 sm:text-center sm:py-4 w-full text-right sm:w-40'>
+            <p className='text-black dark:text-white'>
+              <span className='inline-block sm:hidden me-2'>نام خانوادگی:</span>
               {item.lastName}
             </p>
           </div>
 
-          <div className='p-2 text-center py-4 w-40'>
-            <p className='text-black dark:text-white'>{item.firstName}</p>
+          <div className='p-2 sm:text-center sm:py-4 w-full text-right sm:w-40'>
+            <p className='text-black dark:text-white'>
+              <span className='inline-block sm:hidden me-2'>نام:</span>
+              {item.firstName}
+            </p>
           </div>
 
-          <div className='p-2 text-center py-4 w-40'>
+          {/* <div className='p-2 text-center py-4 w-40'>
             <p className='text-black dark:text-white'>
               {latinToPersianDigit(String(item.payed))}
             </p>
-          </div>
+          </div> */}
 
-          <div className='p-2 text-center py-4 w-40'>
+          {/* <div className='p-2 text-center py-4 w-40'>
             <p className='text-black dark:text-white'>
               {latinToPersianDigit(String(item.notPayed))}
             </p>
-          </div>
+          </div> */}
 
-          <div className='p-2 text-center py-4 w-40'>
-            <p className='text-black dark:text-white' dir='ltr'>
+          <div className='p-2 sm:text-center sm:py-4 w-full text-right sm:w-40'>
+            <p className='text-black dark:text-white'>
+              <span className='inline-block sm:hidden me-2'>شماره تماس:</span>
+
               {item.phone}
             </p>
           </div>
 
-          <div className='p-2 text-center py-4 w-40'>
+          {/* <div className='p-2 text-center py-4 w-40'>
             <p className='text-black dark:text-white'>{item.nationalCode}</p>
-          </div>
+          </div> */}
         </div>
       ))}
     </>
