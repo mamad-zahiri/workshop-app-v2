9a222546b6c73884226af32740cf97f8b0fcb586
Mamad Zahiri <mamadzhri@gmail.com>
2024-10-12 02:32:14 +0330

    update manifest

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,30 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      categories: ["books", "education", "medical"],
       injectRegister: "auto",
       registerType: "autoUpdate",
       workbox: {
         clientsClaim: true,
         skipWaiting: true,
+      },
+      manifest: {
+        name: "مدیریت کارگاه تولید سنسور",
+        short_name: "مدیریت کارگاه",
+        description: "مدیریت کارمندان و تولید کارگاه تولید سنسور",
+        theme_color: "#ffffff",
+        // icons: [
+        //   {
+        //     src: "pwa-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "pwa-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //   },
+        // ],
       },
     }),
   ],
