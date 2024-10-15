4a8a61f23b96cfecd082b7a15ee496eb147fc596
Mamad Zahiri <mamadzhri@gmail.com>
2024-10-15 23:57:57 +0330

    add tumbnail for products table

--- src/pages/Products/ProductsTable.tsx
+++ src/pages/Products/ProductsTable.tsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom"
 import { latinToPersianDigit } from "../../common/helpers"
 import { Product } from "../../types"
 
-const TableOne = ({ products = [] }: { products: Product[] }) => {
+const ProductListTableElm = ({ products = [] }: { products: Product[] }) => {
   return (
     <>
       <div className='flex justify-start bg-slate-100  border-b hover:[&>div]:bg-slate-400 hover:[&>div]:text-white'>
-        <div className='text-center p-4 flex justify-center content-start w-full sm:w-70'>
-          <h5 className='text-sm font-medium uppercase xsm:text-base'>نام</h5>
+        <div className='text-center p-4 flex justify-center content-start w-2/3 sm:w-70'>
+          <h5 className='text-sm font-medium uppercase xsm:text-base'>محصول</h5>
         </div>
 
-        <div className='text-center p-4 flex justify-center content-start w-full sm:w-50'>
+        <div className='text-center p-4 flex justify-center content-start w-1/3 sm:w-50'>
           <h5 className='text-sm font-medium uppercase xsm:text-base'>قیمت فروش</h5>
         </div>
 
@@ -27,11 +27,17 @@ const TableOne = ({ products = [] }: { products: Product[] }) => {
           <Link
             to='/products/edit'
             state={{ product: item }}
-            className='p-2 text-start py-4 w-full sm:w-70 block'>
-            <p className='text-black text-center'>{item.name}</p>
+            className='p-2 text-start py-4 w-2/3 sm:w-70 block'>
+            <p className='text-black text-center'>
+              <img
+                src={item.picture}
+                className='inline-block object-cover rounded me-3 h-10 w-10 align-middle'
+              />
+              {item.name}
+            </p>
           </Link>
 
-          <div className='p-2 text-start py-4 w-full sm:w-50'>
+          <div className='p-2 text-start py-4 w-1/3 sm:w-50'>
             <p className='text-black text-center'>
               {latinToPersianDigit(item.price.toLocaleString())} تومان
             </p>
@@ -48,4 +54,4 @@ const TableOne = ({ products = [] }: { products: Product[] }) => {
   )
 }
 
-export default TableOne
+export default ProductListTableElm
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,25 +1,21 @@
-// import { Modal } from "@mui/material"
 import { Link } from "react-router-dom"
 import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb"
 import { Product } from "../../types"
-import TableOne from "./ProductsTable"
+import ProductListTableElm from "./ProductsTable"
 
 const products: Array<Product> = [
   {
     name: "سوییچ سنسور Tiba",
-    // code: "T",
     picture: "/products/p1.png",
     price: 35000,
   },
   {
     name: "سوییچ سنسور ABS",
-    // code: "A",
     picture: "/products/p2.png",
     price: 22000,
   },
   {
     name: "سوییچ سنسور CBS",
-    // code: "C",
     picture: "/products/p3.png",
     price: 28000,
   },
@@ -30,7 +26,7 @@ const Products = () => {
     <>
       <Breadcrumb pageName='محصولات' />
 
-      <TableOne products={products} />
+      <ProductListTableElm products={products} />
 
       <Link to='/products/add'>
         <div className='fixed bottom-10 left-10 rounded-lg w-10 h-10 bg-cyan-600 flex justify-center p-1.5 hover:cursor-pointer'>
